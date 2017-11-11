5fffa9a700629c8d4382c529023a0a11fd323913
George Flourentzos <georgeflourentzos@gmail.com>
2017-11-11 07:24:59 -0800

    Changed message control from text field to label

--- IJP_app/src/WorldController.java
+++ IJP_app/src/WorldController.java
@@ -3,7 +3,7 @@ import java.util.Iterator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 
 public class WorldController {
@@ -15,7 +15,7 @@ public class WorldController {
 	private ImageView imageView;
 	
 	@FXML
-	private TextField message;
+	private Label message;
 	
 	@FXML
 	private ImageView item1;
